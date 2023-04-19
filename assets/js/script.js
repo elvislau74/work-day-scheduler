53bd5d53a75e6659a2e3c5c1c4635273aa8dc322
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-19 16:04:56 -0400

    delete padding from header in bootstrap class, add current hour variable and timeslots array of objects of id times and hour numbers to write loop using jQuery's .each statement to change colors of each time slot background based on if they are past, present or future hour.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,43 @@
 // in the html.
 $(function () {
   var dateAndTime = $('#currentDay')
-
+  var currentHour = dayjs().format('HH');
+  var timeSlots = [
+    {idTime: $('#hour-9'), hour: 09},
+    {idTime: $('#hour-10'), hour: 10},
+    {idTime: $('#hour-11'), hour: 11},
+    {idTime: $('#hour-12'), hour: 12},
+    {idTime: $('#hour-13'), hour: 13},
+    {idTime: $('#hour-14'), hour: 14},
+    {idTime: $('#hour-15'), hour: 15},
+    {idTime: $('#hour-16'), hour: 16},
+    {idTime: $('#hour-17'), hour: 17},
+  ];
 
   function displayDateAndTime() {
   var currentDateAndTime = dayjs();
-  $(dateAndTime).text(currentDateAndTime.format('[Today is] MMMM D, YYYY, h:mm:ss a'))
+  $(dateAndTime).text(currentDateAndTime.format('[Today is] dddd, MMMM D, YYYY, h:mm:ss a'))
   }
+
+  $.each(timeSlots, function(index, value) {
+    var hourId = value.idTime;
+    if (value.hour < currentHour){
+      $(hourId).removeClass('present');
+      $(hourId).removeClass('future');
+      $(hourId).addClass('past');
+    }
+    else if (value.hour == currentHour) {
+      $(hourId).removeClass('past');
+      $(hourId).removeClass('future');
+      $(hourId).addClass('present');
+    }
+    else {
+      $(hourId).removeClass("past");
+      $(hourId).removeClass("present");
+      $(hourId).addClass("future");
+    }
+  });
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -26,7 +57,6 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
   displayDateAndTime();
   setInterval(displayDateAndTime, 1000);
 
